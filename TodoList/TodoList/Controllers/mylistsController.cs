6dc6e5ae6509dc3d47ec8a6ad88dcba9efb5d5de
Eliwaaaaaaa A <cheahsiongwong@gmail.com>
2019-07-09 18:08:51 +0800

    todolist

---
+++ TodoList/TodoList/Controllers/mylistsController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using TodoList.Models;
+using System.Web.Http.Cors;
+
+namespace TodoList.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class MylistsController : ApiController
+    {
+        private TodolistEntities db = new TodolistEntities();
+
+        // GET: api/mylists
+        public IQueryable<mylist> Getmylists()
+        {
+            return db.mylists;
+        }
+
+        // GET: api/mylists/5
+        [ResponseType(typeof(mylist))]
+        public IHttpActionResult Getmylist(int id)
+        {
+            mylist mylist = db.mylists.Find(id);
+            if (mylist == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(mylist);
+        }
+
+        // PUT: api/mylists/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Putmylist(int id, mylist mylist)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != mylist.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(mylist).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!mylistExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/mylists
+        [ResponseType(typeof(mylist))]
+        public IHttpActionResult Postmylist(mylist mylist)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.mylists.Add(mylist);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = mylist.Id }, mylist);
+        }
+
+        // DELETE: api/mylists/5
+        [ResponseType(typeof(mylist))]
+        public IHttpActionResult Deletemylist(int id)
+        {
+            mylist mylist = db.mylists.Find(id);
+            if (mylist == null)
+            {
+                return NotFound();
+            }
+
+            db.mylists.Remove(mylist);
+            db.SaveChanges();
+
+            return Ok(mylist);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool mylistExists(int id)
+        {
+            return db.mylists.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ TodoList/TodoList/myList/crud.js
@@ -0,0 +1,75 @@
+﻿angular.module('myApp', []).controller('CrudCtrl', ['$scope', 'crudService', function ($scope, crudService) {
+
+    $scope.createVM = {}
+    $scope.updateVM = {}
+    $scope.showAlert = false;
+
+    $scope.loadTableData = function () {
+        crudService.serviceCall("GET", "", "").then(function (response) {
+            if (response.data.length < 1) {
+                $scope.showAlert = true;
+                $scope.alertName = "Note!"
+                $scope.alertMessage = "These Is No Record To Show."
+            }
+            $scope.listData = response.data;
+        });
+    }
+    // add new task
+    $scope.addTask = function (isValid) {
+        if (isValid) {
+            $scope.addPhoneNumberForm.$setPristine();
+            crudService.serviceCall("POST", "", $scope.createVM).then(function (response) {
+                $scope.showAlert = true;
+                $scope.alertName = "Success!"
+                $scope.alertMessage = "Task Is Added Successfully."
+                $scope.createVM = {};
+                $scope.loadTableData();
+            });
+        }
+    }
+    // update task model
+    $scope.openUpdateTaskModal = function (Id) {
+        crudService.serviceCall("GET", Id, "").then(function (response) {
+            $scope.updateVM = response.data;
+        });
+    }
+    
+    $scope.updateTask = function (Id) {
+
+        crudService.serviceCall("PUT", Id, $scope.updateVM).then(function (response) {
+            $scope.showAlert = true;
+            $scope.alertName = "Success!"
+            $scope.alertMessage = "Task Is Updated Successfully."
+            $scope.loadTableData();
+        });
+
+    }
+
+     //delete task
+    $scope.openDeleteTaskModal = function (Id) {
+        $scope.recordIdToDelete = Id;
+    }
+   
+    $scope.deleteTask = function (Id) {
+        crudService.serviceCall("Delete", Id, "").then(function (response) {
+            $scope.showAlert = true;
+            $scope.alertName = "Success!"
+            $scope.alertMessage = "Task Is Deleted Successfully."
+            $scope.loadTableData();
+        });
+    }
+
+    $scope.loadTableData();
+
+}])
+    .service('crudService', ['$http', function ($http) {
+        return {
+            serviceCall: function (method, parameter, data) {
+                return $http({
+                    method: method,
+                    url: 'https://localhost:44312/api/mylists/' + parameter,
+                    data: data
+                });
+            }
+        };
+    }]);
